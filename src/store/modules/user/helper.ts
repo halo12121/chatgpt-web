f8c28c51ba15f640f83b938f587087a2999b2103
halo12121 <125181965+halo12121@users.noreply.github.com>
2023-03-21 11:18:37 +0800

    Update helper.ts
    修改logo

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/125181965',
+      name: 'HI',
+      description: '仅供内部体验，切勿外传',
     },
   }
 }
